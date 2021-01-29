5bb22d12bc469e42da3b36dd8f92b0e3f7432a61
Gabriel Oswaldo <gabriel.oswaldo99@gmail.com>
2021-01-29 15:36:27 -0300

    feat: create GenericInput component
    wip
    
    wip

--- src/Components/GenericInput/index.tsx
+++ src/Components/GenericInput/index.tsx
@@ -1,10 +1,47 @@
-import { Text, View} from 'native-base';
+import {Text, View, Input, Item} from 'native-base';
 import React from 'react';
+import Styles from './styles';
 
-const GenericInput: React.FunctionComponent = () => {
+type IProps = {
+  disabled?: boolean;
+  error?: string;
+  hidePassword?: boolean;
+  keyboardType: 'default' | 'numeric';
+  label?: string;
+  maxLength?: number;
+  placeholder: string;
+  value: string;
+  onChange: any;
+};
+
+const GenericInput: React.FunctionComponent<IProps> = (props: IProps) => {
+  const hasError = (): boolean => {
+    return Boolean(props.error?.length);
+  };
+
   return (
     <View>
-      <Text>GenericInput</Text>
+      {props.label ? (
+        <Text style={Styles().labelStyle}>{props.label}</Text>
+      ) : (
+        ''
+      )}
+      <Item underline style={Styles({error: hasError()}).underlineInputError}>
+        <Input
+          style={Styles().inputStyle}
+          disabled={props.disabled ? props.disabled : false}
+          secureTextEntry={props.hidePassword ? props.hidePassword : false}
+          keyboardType={props.keyboardType}
+          maxLength={props.maxLength ? props.maxLength : 255}
+          placeholder={props.placeholder}
+          placeholderTextColor={'rgba(252, 252, 252, 0.5)'}
+          value={props.value}
+          onChangeText={props.onChange}
+        />
+      </Item>
+      {props.error ? (
+        <Text style={Styles().messageInputError}>{props.error}</Text>
+      ) : null}
     </View>
   );
 };
