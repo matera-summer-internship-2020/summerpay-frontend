af821b5f3cae8acec292a6ce9e01f3df4958150a
gdseabra <gdseabra@gmail.com>
2021-02-01 09:39:39 -0300

    fix: restore original Login/index

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,12 +1,10 @@
 import { Container, Text} from 'native-base';
 import React from 'react';
-import AccountInfo from '../../Components/AccountInfo';
 
 const Login: React.FunctionComponent = () => {
   return (
     <Container>
       <Text>Login</Text>
-      <AccountInfo userName="Gabriel Seabra"/>
     </Container>
   );
 };
