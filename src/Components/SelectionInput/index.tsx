fba9743d8c302f1df545ee27727bc337a9dc277f
Francisco <franciscogupe@gmail.com>
2021-02-01 09:17:22 -0300

    creates SelectionInput component & style

--- src/Components/SelectionInput/index.tsx
+++ src/Components/SelectionInput/index.tsx
@@ -1,10 +1,44 @@
-import { Text, View} from 'native-base';
+import {View, Item, Picker, Icon} from 'native-base';
 import React from 'react';
+import Styles from './styles'
 
-const SelectionInput: React.FunctionComponent = () => {
+type dataValue = {
+  label: string;
+  value: string;
+}
+
+interface Iprops {
+  data: any[];
+  selectedValue: (value: string) => void;
+}
+
+const SelectionInput: React.FunctionComponent<Iprops> = (props: Iprops) => {
+  
+  const [selectedValue, setSelectValue] = React.useState<string>('');     // State
+
+  const changeValue = (value: string):void  => {  // Changes the selected value
+    setSelectValue(value);
+    props.selectedValue(value);
+  }   
+
   return (
     <View>
-      <Text>SelectionInput</Text>
+      <Item picker>
+        <Picker
+          mode="dropdown"
+          iosIcon={<Icon name="arrow-down" />}
+          style={Styles.pickerStyle}
+          placeholder="Select"
+          selectedValue={selectedValue}  
+          onValueChange={changeValue}   
+        >
+          {props.data.map((dataValue) => {          // dataValue list
+            return (
+              <Picker.Item label={dataValue.label} value={dataValue.value} key={dataValue.label}/>
+            )
+          })}
+        </Picker>
+      </Item>
     </View>
   );
 };
---
+++ src/Components/SelectionInput/styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import { color } from 'react-native-reanimated';
+
+const Styles = StyleSheet.create( {
+  pickerStyle: {
+      width: '100%',
+      height: 40,
+      color: "#00fa"
+  }
+})
+
+export default Styles
