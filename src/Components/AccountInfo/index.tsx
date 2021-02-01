1d4dfc284ab90d24fbff61a8bb128679738c5625
gdseabra <gdseabra@gmail.com>
2021-02-01 15:54:12 -0300

    refactor: remove unused imports

--- src/Components/AccountInfo/index.tsx
+++ src/Components/AccountInfo/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {Text, View} from 'react-native';
 import Styles from './styles';
 
 type IProps = {
