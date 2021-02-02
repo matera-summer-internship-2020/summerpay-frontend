e8093c29ce13ef1bb96cfb09a0035ec758674ac0
Flavia Ferreira Domingues <flavia.domingues@matera.com>
2021-02-02 15:37:01 -0300

    Adicionando ButtonComponent

---
+++ src/Components/ButtonComponent/index.tsx
@@ -0,0 +1,24 @@
+import { Button, Text } from 'native-base';
+import React from 'react';
+
+import { Styles } from './styles';
+
+type IProps = {
+  disabled: boolean;
+  mainButton: boolean;
+  text: string;
+  onPress: any;
+  size: 's' | 'm' | 'l';
+};
+
+const ButtonComponent: React.FunctionComponent<IProps> = (props: IProps) => (
+  <Button
+    disabled={props.disabled}
+    onPress={props.onPress}
+    style={Styles({ disabled: props.disabled, mainButton: props.mainButton, size: props.size }).button}
+  >
+    <Text style={Styles({ mainButton: props.mainButton }).text}>{props.text}</Text>
+  </Button>
+);
+
+export default ButtonComponent;
---
+++ src/Components/ButtonComponent/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+type IProps = {
+  disabled?: boolean;
+  mainButton?: boolean;
+  size?: string;
+};
+
+export const Styles = (props?: IProps) =>
+  StyleSheet.create({
+    button: {
+      width: getWidthButton(props?.size),
+      height: 65,
+      borderColor: '#CC7600',
+      borderWidth: 1,
+      backgroundColor: props?.mainButton ? '#CC7600' : '#FFF',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      borderRadius: 10,
+      opacity: props?.disabled ? 0.7 : 1
+    },
+    text: {
+      color: props?.mainButton ? '#FFF' : '#CC7600',
+      fontSize: 16
+    }
+  });
+
+const getWidthButton = (size?: string): string => {
+  if (size) {
+    switch (size) {
+      case 's':
+        return '40%';
+
+      case 'm':
+        return '60%';
+
+      case 'l':
+        return '75%';
+
+      default:
+        return '60%';
+    }
+  }
+
+  return '60%';
+};
