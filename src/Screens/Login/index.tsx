4ef998556a051dacc837c6c3aa687a05e397305d
izumizawa <mariana.izumizawa@matera.com>
2021-02-03 10:37:20 -0300

    feat: update and style svg images and add button component

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -8,18 +8,16 @@ import Styles from './styles';
 import GenericInput from '../../Components/GenericInput';
 import {cpfMask} from '../../Helpers/Masks';
 import Logo from '../../Assets/Images/logo.svg';
+import LoginBackground from '../../Assets/Images/login-background.svg';
+import ButtonComponent from '../../Components/ButtonComponent';
 
 const Login: React.FunctionComponent = () => {
   const centralNavigationService = CentralNavigationService<AppStackParamList>();
   const [cpfValue, setCpfValue] = React.useState<string>('');
   const [passwordValue, setPasswordValue] = React.useState<string>('');
-
+  
   const disableLogin = (cpfValue: string, passwordValue: string) => {
-    if (cpfValue.length === 14 && passwordValue.length !== 0) {
-      return false;
-    } else {
-      return true;
-    }
+    return (cpfValue.length !== 14 || passwordValue.length === 0)
   };
 
   const onCpfChange = (value: string): void => {
@@ -31,45 +29,48 @@ const Login: React.FunctionComponent = () => {
   };
 
   return (
-    <Container style={Styles().mainContainer}>
-      <Logo height={'200'} width={'200'} />
-      <View style={Styles().viewFakeLogo}/>
-      <View style={Styles().viewLoginData}>
-      <Text style={Styles().textTitle}>Acesse sua conta</Text>
-      <GenericInput 
-        disabled={false}
-        keyboardType={'numeric'}
-        label={'CPF'}
-        maxLength={14}
-        placeholder={'000.000.000-00'}
-        value={cpfMask(cpfValue)}
-        onChange={onCpfChange}
-        />
-      <View style={Styles().viewSpaceBetween}/>
-      <GenericInput 
-        disabled={false}
-        hidePassword={true}
-        keyboardType={'numeric'}
-        label={'Senha'}
-        placeholder={'-   -   -   -   -   -'}
-        value={passwordValue}
-        onChange={onPasswordChange}
-        />
-      <Button
-      disabled={disableLogin(cpfValue, passwordValue)}
-      onPress={() => void {}} 
-      style={Styles({disabledButton: disableLogin(cpfValue, passwordValue)}).buttonLogin}>
-        <Text style={Styles().textButtonLogin}>Entrar</Text>
-      </Button>
+    <Container style={Styles.mainContainer}>
+      <LoginBackground width={'100%'} height={'100%'} style={Styles.imageBackground}/>
+      <View style={Styles.viewLogo}>
+        <Logo width={'200'} />
       </View>
-      <Text style={Styles().textSignIn}>Não possui conta ainda?</Text>
+      <View style={Styles.viewLoginData}>
+        <Text style={Styles.textTitle}>Acesse sua conta</Text>
+        <GenericInput 
+          disabled={false}
+          keyboardType={'numeric'}
+          label={'CPF'}
+          maxLength={14}
+          placeholder={'000.000.000-00'}
+          value={cpfMask(cpfValue)}
+          onChange={onCpfChange}
+          />
+        <View style={{height: 16}}/>
+        <GenericInput 
+          disabled={false}
+          hidePassword={true}
+          keyboardType={'numeric'}
+          label={'Senha'}
+          placeholder={'-   -   -   -   -   -   -   -'}
+          value={passwordValue}
+          onChange={onPasswordChange}
+          />
+        <View style={{height: 32}}/>
+        <ButtonComponent 
+        size={'l'} 
+        disabled={disableLogin(cpfValue, passwordValue)} 
+        mainButton={true} 
+        text={'Entrar'} 
+        onPress={() => void {}}
+        />
+      </View>
+      <Text style={Styles.textSignIn}>Não possui conta ainda?</Text>
       <TouchableOpacity
-      onPress={() => centralNavigationService.navigate('Onboarding')} 
-      style={Styles().buttonSignIn}
+      onPress={() => void {}} 
+      style={Styles.buttonSignIn}
       >
-        <Text style={Styles().textButtonSignIn}>Cadastre-se aqui</Text>
+        <Text style={Styles.textButtonSignIn}>Cadastre-se aqui</Text>
       </TouchableOpacity>
-
     </Container>
   );
 };
--- src/Screens/Login/styles.ts
+++ src/Screens/Login/styles.ts
@@ -1,33 +1,24 @@
 import {StyleSheet} from 'react-native';
 
-type IProps = {
-    disabledButton?: boolean;
-};
-
-const Styles = (props?: IProps) => StyleSheet.create({
+const Styles = StyleSheet.create({
 
 mainContainer: {
     backgroundColor: '#002D80',
     justifyContent: 'center'
 },
 
-viewFakeLogo: {
-    borderRadius: 100,
-    borderColor: '#CC7600',
-    borderWidth: 10,
-    width: 125,
-    height: 125,
-    alignSelf: "center"
+imageBackground: {
+    position: 'absolute',
+    alignSelf: 'center',   
+},
+
+viewLogo:{
+    alignSelf: 'center',
 },
 
 viewLoginData: {
-    // backgroundColor: 'green',
-    marginVertical: 32,
-    marginHorizontal: 80,
-},
-
-viewSpaceBetween: {
-    height: 16
+    marginBottom: '5%',
+    marginHorizontal: '20%',
 },
 
 textTitle: {
@@ -37,36 +28,13 @@ textTitle: {
     marginBottom: 16
 },
 
-buttonLogin: {
-    backgroundColor: '#CC7600',
-    borderRadius: 10,
-    shadowRadius: 4,
-    shadowColor: '#000',
-    shadowOpacity: 1,
-    shadowOffset: {height: 4, width: 4},
-    elevation: 2,
-    width: '100%',
-    minHeight: 56,
-    justifyContent: 'center',
-    marginTop: 32,
-    opacity: props && props.disabledButton ? 0.5 : 1,
-},
-
-textButtonLogin: {
-    color: '#FCFCFC',
-    fontSize: 20,
-    textTransform: 'capitalize',
-},
-
 textSignIn: {
-    // backgroundColor: 'pink',
     color: '#FCFCFC',
     fontSize: 14,
     textAlign: "center"
 },
 
 buttonSignIn: {
-    // backgroundColor: 'red',
     marginTop: 8
 },
 
