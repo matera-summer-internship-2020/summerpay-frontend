bcf16f7c1be001d13477bb9de34f4671f0d57528
gdseabra <gdseabra@gmail.com>
2021-02-03 09:33:41 -0300

    feature: create name input screen

--- src/Screens/Onboarding/Name/index.tsx
+++ src/Screens/Onboarding/Name/index.tsx
@@ -1,10 +1,29 @@
-import { Container, Text } from 'native-base';
+import { Container, Text, View } from 'native-base';
 import React from 'react';
+import ButtonComponent from '../../../Components/ButtonComponent';
+import GenericInput from '../../../Components/GenericInput';
+import ProgressBar from '../../../Components/ProgressBar';
+import Styles from './styles';
 
 const Name: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = React.useState<string>('');
+
+  const onChange = (value: string): void => {
+    setInputValue(value);
+  };
+
   return (
-    <Container>
-      <Text>Name</Text>
+    <Container style={Styles.container}>
+      <ProgressBar currentStep={1} numberOfSteps={7}/>
+      <Container style={Styles.inputAndLabelContainer}>
+        <Text style={Styles.instructionText}>Para começar, qual é o seu nome completo?</Text>
+        <View style={Styles.inputView}>
+          <GenericInput keyboardType='default' placeholder='Nome completo' error={
+              inputValue.length < 2 ? 'Nome inválido' : ''
+            } value={inputValue} onChange={onChange}/>
+        </View>
+        <ButtonComponent disabled={inputValue.length < 2} mainButton={true} text="Próximo" size='m' onPress={() => {}}/>
+      </Container>
     </Container>
   );
 };
