7e784cd787e3997e66e00adea707995f9e3d76a7
leobur <leonardo.burrone@matera.com>
2021-02-08 18:26:21 -0300

    Configure api service

--- src/Ducks/Login/Sagas.ts
+++ src/Ducks/Login/Sagas.ts
@@ -1,6 +1,14 @@
 import { all, takeLatest, call, put, select } from 'redux-saga/effects';
 
+import api from '../../Services/Api';
+
 import { LoginActionTypes } from './types';
+
+/*
+* Exemplo de uso da "Api":
+1) GET: yield call(api.get, '/profile');
+2) POST: yield call(api.post, '/login', objetoComUsuarioSenha);
+*/
 
 export function* login(action: any) {
   // TODO: recuperar usuário (do Reducer) e senha (da Action)
--- src/Ducks/Onboarding/Sagas.ts
+++ src/Ducks/Onboarding/Sagas.ts
@@ -1,6 +1,14 @@
 import { all, takeLatest, call, put, select } from 'redux-saga/effects';
 
+import api from '../../Services/Api';
+
 import { OnboardingActionTypes } from './types';
+
+/*
+* Exemplo de uso da "Api":
+1) GET: yield call(api.get, '/profile');
+2) POST: yield call(api.post, '/login', objetoComUsuarioSenha);
+*/
 
 export function* createAccount(action: any) {
   // TODO: recuperar dados de cadastro do Reducer
