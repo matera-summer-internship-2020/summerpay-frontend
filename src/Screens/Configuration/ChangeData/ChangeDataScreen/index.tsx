faf8fade146ebc922d0a151506d1af4113b7f5cc
gdseabra <gdseabra@gmail.com>
2021-02-12 14:30:46 -0300

    feat: create static change data screen

--- src/Screens/Configuration/ChangeData/ChangeDataScreen/index.tsx
+++ src/Screens/Configuration/ChangeData/ChangeDataScreen/index.tsx
@@ -1,12 +1,42 @@
-import { Container, Text } from 'native-base';
+import { Container, Text, View } from 'native-base';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 
 import Styles from './styles';
 
 const ChangeData: React.FunctionComponent = () => {
   return (
-    <Container>
-      <Text>ChangeData</Text>
+    <Container style={Styles.container}>
+      <View style={Styles.boxView}>
+        <View style={Styles.firstRow}>
+          <Text style={Styles.boxTitle}>Dados pessoais</Text>
+          <TouchableOpacity>
+            <Text style={Styles.edit}>Editar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={Styles.boxContent}>
+          <Text style={Styles.contentTitle}>Letícia Nunes</Text>
+
+          <Text style={Styles.contentText}>123.456.789-10</Text>
+          <Text style={Styles.contentText}>(19) 99976-8133</Text>
+          <Text style={Styles.contentText}>Casado(a)</Text>
+        </View>
+      </View>
+      <View style={Styles.boxView}>
+        <View style={Styles.firstRow}>
+          <Text style={Styles.boxTitle}>Endereço</Text>
+          <TouchableOpacity>
+            <Text style={Styles.edit}>Editar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={Styles.boxContent}>
+          <Text style={Styles.contentTitle}>Av. Oscar Pedroso, 1932</Text>
+
+          <Text style={Styles.contentText}>apto. 54, próximo ao posto</Text>
+          <Text style={Styles.contentText}>Campinas, SP</Text>
+          <Text style={Styles.contentText}>13028-192</Text>
+        </View>
+      </View>
     </Container>
   );
 };
--- src/Screens/Configuration/ChangeData/ChangeDataScreen/styles.ts
+++ src/Screens/Configuration/ChangeData/ChangeDataScreen/styles.ts
@@ -1,5 +1,43 @@
 import { StyleSheet } from 'react-native';
 
-const Styles = StyleSheet.create({});
+const Styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#002D80',
+    alignItems: 'center',
+    paddingTop: 32
+  },
+  boxView: {
+    backgroundColor: '#FCFCFC',
+    width: '80%',
+    padding: 16,
+    marginTop: 8,
+    marginBottom: 8,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#CC7600'
+  },
+  boxTitle: {
+    fontSize: 16,
+    color: '#CC7600'
+  },
+  edit: {
+    fontSize: 12,
+    color: '#CC7600'
+  },
+  firstRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16
+  },
+  boxContent: {},
+  contentTitle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  contentText: {
+    fontSize: 14,
+    marginTop: 4
+  }
+});
 
 export default Styles;
