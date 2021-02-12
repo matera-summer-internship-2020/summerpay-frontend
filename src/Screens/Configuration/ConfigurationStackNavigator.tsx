340eba709a30b812096b8641a64cc1631e3ade46
izumizawa <mariana.izumizawa@matera.com>
2021-02-12 16:04:08 -0300

    feat: create delete account screen

--- src/Screens/Configuration/ConfigurationStackNavigator.tsx
+++ src/Screens/Configuration/ConfigurationStackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 
 import ConfigurationScreen from './ConfigurationScreen';
+import DeleteAccount from './DeleteAccount';
 
 import { ConfigurationStackParamList } from './types';
 
@@ -14,6 +15,18 @@ const ConfigurationNavigator = () => (
       component={ConfigurationScreen}
       options={{
         headerTitle: 'Configurações',
+        headerStyle: {
+          backgroundColor: '#002D80'
+        },
+        headerTintColor: '#FFF',
+        headerHideShadow: true
+      }}
+    />
+    <Stack.Screen
+      name="DeleteAccount"
+      component={DeleteAccount}
+      options={{
+        headerTitle: 'Deletar conta',
         headerStyle: {
           backgroundColor: '#002D80'
         },
