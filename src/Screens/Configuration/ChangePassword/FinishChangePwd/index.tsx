6711a1c7d66f8028d20d59e1daf03f0be282860a
Gabriel Oswaldo <gabriel.oswaldo99@gmail.com>
2021-02-23 22:49:25 -0300

    feat: create FinishChangePwd screen

--- src/Screens/Configuration/ChangePassword/FinishChangePwd/index.tsx
+++ src/Screens/Configuration/ChangePassword/FinishChangePwd/index.tsx
@@ -1,12 +1,28 @@
-import { Container, Text } from 'native-base';
+import { Container, Text, View } from 'native-base';
+
 import React from 'react';
+
+import ConfirmIcon from '../../../../Assets/Images/confirm.svg';
+import ButtonComponent from '../../../../Components/ButtonComponent';
+import { CentralNavigationService } from '../../../../Services/Navigation';
+import { ConfigurationStackParamList } from '../../types';
 
 import Styles from './styles';
 
 const FinishChangePwd: React.FunctionComponent = () => {
+  const configurationNavigationService = CentralNavigationService<ConfigurationStackParamList>();
+
+  const onOkButtonPress = (): void => {
+    configurationNavigationService.reset('ConfigurationScreen');
+  };
+
   return (
-    <Container>
-      <Text>FinishChangePwd</Text>
+    <Container style={Styles.container}>
+      <View style={Styles.confirmIconAndTextView}>
+        <ConfirmIcon width={'18%'} height={'100%'} />
+        <Text style={Styles.confirmText}>Senha alterada com sucesso!</Text>
+      </View>
+      <ButtonComponent disabled={false} mainButton={true} text={'OK'} onPress={onOkButtonPress} size={'m'} />
     </Container>
   );
 };
