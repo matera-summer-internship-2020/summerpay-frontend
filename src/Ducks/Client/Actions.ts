39fc8c8138431c24cc78d16465d5534485dbb0e1
Francisco <franciscogupe@gmail.com>
2021-02-26 10:40:57 -0300

    fix: ESLint warning

--- src/Ducks/Client/Actions.ts
+++ src/Ducks/Client/Actions.ts
@@ -1,6 +1,6 @@
-import { ClientActionTypes } from './types';
-
 import { Client, Account } from '../../types';
+
+import { ClientActionTypes } from './types';
 
 export const saveClientIdAction = (payload: { clientData: Client; accountData: Account }) => ({
   payload,
